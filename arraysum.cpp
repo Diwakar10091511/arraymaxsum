64d0a03f7b636a5695ab5daaeddef88c89307429
Diwakar <dfb1009@gmail.com>
2021-02-01 16:39:13 +0530

    first comments added

--- arraysum.cpp
+++ arraysum.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main()
 {
-	int s;
+	//here we are taking an input of array.
+	int s=0;
 	int n;
 	cin>>n;
 	int a[n];
@@ -10,6 +11,7 @@ int main()
 	{
 		cin>>a[i];
 	}
+	// process of sorting starting from here.
 	for(int i=n-1;i>0;i--)
 	{
 		for(int j=0;j<i;j++)
@@ -23,20 +25,21 @@ int main()
 			}
 		}
 	}
+	// sorted array is getting printed
 	for(int i=0;i<n;i++)
 	{
 		cout<<a[i]<<endl;
 	}
 for(int i=0;i<n;i++)
 {
-	if(a[i]>0)
+	if(a[i]>0) // checking for the first positive index
 	{
 		s=i;
 		break;
 	}
 }
 int p=0;
-for(int i=s;i<n;i++)
+for(int i=s;i<n;i++)  // finding the sum from that positive value and so on
 {
 p=p+a[i];
 cout<<a[i];
@@ -44,4 +47,4 @@ cout<<a[i];
 cout<<"the sum is"<<p;
 return 0;
 }
-
+// happy programming.
